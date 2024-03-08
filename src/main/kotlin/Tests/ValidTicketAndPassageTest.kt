57814798cc65f302d75e758b99359de6ac83b50a
Flatbaby <xxflatbabyxx@gmail.com>
2024-03-08 21:14:13 +0100

    Fixes

--- src/main/kotlin/Tests/ValidTicketAndPassageTest.kt
+++ src/main/kotlin/Tests/ValidTicketAndPassageTest.kt
@@ -7,20 +7,20 @@ import com.skidata.checkpoint.dt.CheckpointIdentity
 import com.skidata.checkpoint.dt.PassageDirection
 import io.kotest.matchers.shouldBe
 
-class validTicketTest(private val checkpointIdentity : CheckpointIdentity ) : IntegrationTest<BaseSentences> {
-    override suspend fun testFunc(venueController: VenueIntegrationTestController<BaseSentences>){
+class ValidTicketAndPassageTest(private val checkpointIdentity: CheckpointIdentity ) : IntegrationTest<BaseSentences> {
+    override suspend fun specifyTest(venueController: VenueIntegrationTestController<BaseSentences>){
         venueController.withDevice(checkpointIdentity){
             `barrier passability`(PassageDirection.DEFAULT) shouldBe false
             `current display content should eventually contain`(DEFAULT_HSH_IDLE_DISPLAY_CONTENT)
-            var utId= “SD09.24.201300000001”;
+            val utId= "SD09.24.201300000001"
             `offer ticket`(createBarcodeDataCarrier(utId))
             `current display content should eventually contain`(DEFAULT_HSH_GRANT_ACCESS_DISPLAY_CONTENT)
-            //this is to check if the barrier is passable at the moment
-            `barrier passability should stay`(true);
+            // this is to check if the barrier is passable at the moment
+            `barrier passability should stay`(true)
             `pass barrier`(PassageType.SINGLE)
-            `current display content should eventually contain`(DEFAULT_HSH_IDLE_DISPLAY_CONTENT)
-            //this is to check that the barrier will close afterwards.
+            // this is to check that the barrier will close afterward.
             `barrier passability should eventually be`(false)
+            `current display content should eventually contain`(DEFAULT_HSH_IDLE_DISPLAY_CONTENT)
         }
     }
 }
